931ba2b94ae976583049216bb5bd88439d601b66
Namgyu Ho <itsnamgyu@gmail.com>
2022-03-21 13:09:48 +0900

    [Minor] Crop student images to 1:1 ratio

---
+++ core/static/core/js/students.js
@@ -0,0 +1,7 @@
+$(function () {
+  $(".student-image").css("height", $(".student-image").width());
+})
+
+$(window).resize(function () {
+  $(".student-image").css("height", $(".student-image").width());
+})
