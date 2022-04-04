64cbba088f2cf89c77658de8af3d4604e482f40a
Namgyu Ho <itsnamgyu@gmail.com>
2022-04-04 16:01:43 +0900

    Add make_public admin action

--- core/admin.py
+++ core/admin.py
@@ -13,31 +13,45 @@ class UserAdmin(admin.ModelAdmin):
 class PublicationTagAdmin(admin.ModelAdmin):
     exclude = ["last_modified_by"]
 
-
     def save_model(self, request, obj, form, change):
         obj.last_modified_by = request.user
         super().save_model(request, obj, form, change)
 
 
+def make_public(modeladmin, request, queryset):
+    queryset.update(public=True)
+
+
+make_public.short_description = 'Mark selected content as public'
+
+
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ("title", "authors", "venue", "published_date", "created_date")
-    list_filter = ("type", )
+    list_filter = ("type",)
+
+    actions = [make_public]
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ("__str__", "members", "organization", "start_date", "end_date")
 
+    actions = [make_public]
+
 
 @admin.register(Award)
 class AwardAdmin(admin.ModelAdmin):
     list_display = ("name", "awardees", "venue", "awarded_date", "created_date")
 
+    actions = [make_public]
+
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ("__str__", "created_by", "created_date", "image")
+
+    actions = [make_public]
 
 
 @admin.register(InvitationCode)
