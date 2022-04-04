44924c446f036b494d6fe95509ef054072d74a29
Namgyu Ho <itsnamgyu@gmail.com>
2022-04-04 17:25:51 +0900

    Update project page

--- core/static/core/js/projects.js
+++ core/static/core/js/projects.js
@@ -1,23 +1,11 @@
+function resizeProjectImages() {
+  $(".project-image").css("height", $(".project-image").width() / 16 * 9);
+}
+
 $(function() {
-  // $(".collapse")
-  // $(".project-summary-toggle")
-  $(".project-summary").attr("style", "opacity: 0;");
+  resizeProjectImages();
+});
 
-  $(".project-summary").on("hide.bs.collapse", function() {
-    $(this).attr("style", "opacity: 0;");
-    const id = $(this).attr("id");
-    const toggle = $('.project-summary-toggle[href="#' + id + '"]')
-    toggle.text("See More")
-  });
-
-  $(".project-summary").on("shown.bs.collapse", function() {
-    $(this).attr("style", "opacity: 1;");
-  });
-
-  $(".project-summary").on("show.bs.collapse", function() {
-    const id = $(this).attr("id");
-    const toggle = $('.project-summary-toggle[href="#' + id + '"]')
-    toggle.text("See Less")
-  })
-
-})
+$(window).resize(function () {
+  resizeProjectImages();
+});
