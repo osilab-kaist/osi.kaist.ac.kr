5d421eb71a33371016a2d4575c998c910e7be617
Namgyu Ho <itsnamgyu@gmail.com>
2023-08-21 15:35:17 +0900

    Add staticfiles url for deployment

--- osilab_homepage/urls.py
+++ osilab_homepage/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.static import serve
 from django.urls import re_path, path
@@ -10,4 +11,4 @@ urlpatterns = [
     url(r'^osi-management/', admin.site.urls),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('summernote/', include('django_summernote.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
