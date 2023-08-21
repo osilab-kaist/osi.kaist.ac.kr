5031895858d2ded1a5642f7ef20fa823083c0fdb
Namgyu Ho <itsnamgyu@gmail.com>
2023-08-21 15:33:57 +0900

    Minor - hide server info for security?

--- osilab_homepage/settings.py
+++ osilab_homepage/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.HideServerMiddleware',
 ]
 
 ROOT_URLCONF = 'osilab_homepage.urls'
